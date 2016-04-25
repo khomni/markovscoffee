ffcc649d4bde32c8e7af50693b75dcee82477cfc
Jan <a70mik@gmail.com>
2016-04-25 11:53:17 -0700

    prepare markovian output working but untested ;)
    move fast and break things, they said.

--- data/prepare_output.py
+++ data/prepare_output.py
@@ -1,21 +1,35 @@
-import os#, sys
+import os, logging, string
 
-#reload(sys)
-#sys.setdefaultencoding('utf8')
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 markovian_sentences = []
 output = ""
 
-with open("output_02015-12-16_.txt", "r+", encoding="latin-1") as markovian_text:
-    #generate markovian text array
-    for line in markovian_text:
-        markovian_sentences.append(line)
+def filter_printable(str):
+    return "".join([word for word in str if word in string.printable])
 
-with open("current.txt", "r+") as current:
-    #get position from current.txt file
-    current_text = current.read()
-    print(current_text)
-    current_position = int(current_text.split()[0])
-    next_position = (current_position + 1)
-    output = markovian_sentences[current_position]
-    current.write(str(next_position) + "\n" + markovian_sentences[current_position])
+if __name__ == "__main__":
+    with open("output_02015-12-16_.txt", "r+", encoding="latin-1") as markovian_text:
+        #generate markovian text array
+        #this is O(n) and could be O(1) if we felt like being more OCD
+        for line in markovian_text:
+            markovian_sentences.append(line)
+
+    if not os.path.exists("current.mlog"):
+        with open("current.mlog", "w+", encoding="utf-8") as new_file:
+            new_file.write("0")
+
+    with open("current.mlog", "r+", encoding="utf-8") as current:
+        #get position from current.txt file
+        current_text = filter_printable(str(current.read()))
+        logger.info("current position in sentences list = " + current_text)
+        current_position = int(current_text.split()[0])
+        next_position = (current_position + 1)
+        current.truncate(0)
+        output = filter_printable(markovian_sentences[current_position])
+        written_to_file = str(next_position) + "\n" + markovian_sentences[current_position]
+        logger.info("writing to file: " + written_to_file)
+        current.write(written_to_file)
+
+
