e67c7d39465892de1db2dd8990baf101fcdfb04e
Kyle <kylejhalvorson@gmail.com>
2016-05-05 13:16:06 -0700

    restrict tweeting to production env, add debugging break points

--- chron/statusUpdate.js
+++ chron/statusUpdate.js
@@ -10,35 +10,37 @@ var statusUpdate = function(app) {
   var chain = new Chain(dictionary);
 
   var client = app.get('config').twitter
+
+  var Tweet = function(body) {
+    this.body = body;
+    this.validate = function(){
+      return this.body && (this.body.length <= 140) && (this.body.length >= 3) && this.depth === 0
+    };
+    this.format = function(){
+      this.body = this.body.slice(this.body.search(/\S*\w+.\S*/))
+      this.body = this.body[0].toUpperCase() + this.body.slice(1);
+      this.depth = 0;
+      for(c in this.body) {
+        if (this.body[c] ==='(') this.depth++
+        if (this.body[c] ===')') this.depth--
+      }
+      while(this.depth>0) {
+        this.body = [body.slice(0, -1), ')', this.body.slice(-1)].join('');
+        console.log('[FORMAT] adding parenthasis:',this.body)
+        this.depth--
+      }
+      return this
+    }
+    this.addHashtags = function() {
+      // TODO: hashtags
+      return this
+    }
+  }
+  var frequency = "* * * /4"
+  debugger;
   timexe("* * * /4", function() {
 
     var coffeeMarkov = null
-
-    var Tweet = function(body) {
-      this.body = body;
-      this.validate = function(){
-        return this.body && (this.body.length <= 140) && (this.body.length >= 3) && this.depth === 0
-      };
-      this.format = function(){
-        this.body = this.body.slice(this.body.search(/\S*\w+.\S*/))
-        this.body = this.body[0].toUpperCase() + this.body.slice(1);
-        this.depth = 0;
-        for(c in this.body) {
-          if (this.body[c] ==='(') this.depth++
-          if (this.body[c] ===')') this.depth--
-        }
-        while(this.depth>0) {
-          this.body = [body.slice(0, -1), ')', this.body.slice(-1)].join('');
-          console.log('[FORMAT] adding parenthasis:',this.body)
-          this.depth--
-        }
-        return this
-      }
-      this.addHashtags = function() {
-        // TODO: hashtags
-        return this
-      }
-    }
 
     var attempts = 0
     do {
@@ -49,13 +51,17 @@ var statusUpdate = function(app) {
     while(!coffeeMarkov.validate());
 
     console.log("["+coffeeMarkov.body.length+"]",coffeeMarkov.body);
+    debugger;
 
-    client.post('statuses/update', {status: coffeeMarkov}, function(error, tweet, response) {
-      if(error){
-        console.error(error);
-      }
-      console.log(tweet);
-    })
+    if(process.env.NODE_ENV === 'production') {
+      client.post('statuses/update', {status: coffeeMarkov}, function(error, tweet, response) {
+        if(error){
+          console.error(error);
+        }
+        console.log(tweet);
+      })
+    }
+
   });
 }
 
